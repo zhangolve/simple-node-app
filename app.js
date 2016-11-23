e92f7f2262bd43222e527676280d93687920096b
zhangxingwang <zhangxingwang@sinosoft.com.cn>
2016-11-23 16:39:39 +0800

    this is the first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const $ = require('jquery');
+const _=require('lodash');
+'use strict';
+// import $ from jquery;
+_.times(10,function(n){console.log(n)});
+console.log(a);
+$('h1').css({ color: 'red' });
+

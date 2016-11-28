c41624a958b489a5858484f2414cac589c9e79a7
zhangxingwang <zhangxingwang@sinosoft.com.cn>
2016-11-28 14:30:16 +0800

    添加loader，加载babel，css等

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const $ = require('jquery');
-const _=  require('lodash');
-
-
-_.times(10,function(n){console.log(n)});
-let a='this is a test';
-console.log(a);
-$('h1').css({ color: 'green' });
+//const $ = require('jquery');
+import $ from 'jquery';
+import _  from 'lodash' ;
+import './index.css';
+const a = [1, 2, 3, 4, 5, 6, 7, 8];
+const item = _.filter(a, (items) => items > 4);
+console.log(item);
+$('h1').css({ color: 'blue' });
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,25 @@ module.exports = {
   },
   
   output: {
+    
     path: path.resolve(__dirname, "build"),
     publicPath: "/",
     filename: "bundle.js"
-  }
+  },
+   module: {
+  loaders: [
+            //.css 文件使用 style-loader 和 css-loader 来处理
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',//在webpack的module部分的loaders里进行配置即可
+        query: {
+          presets: ['es2015']
+        }
+      }
+           
+        ]
+    }
+
 };

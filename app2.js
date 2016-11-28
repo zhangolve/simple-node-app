fe72a42123c9863ca79a6b42488c4c0c422af53a
zhangxingwang <zhangxingwang@sinosoft.com.cn>
2016-11-28 16:29:01 +0800

    多个输出路径，webpack配置修改

--- app2.js
+++ app2.js
@@ -1,9 +1,10 @@
 // const $ = require('jquery');
 import $ from 'jquery';
 import _ from 'lodash' ;
-// import './index.css';
-const a = [1, 2, 3, 4, 5, 6, 7, 8];
+import './index.css';
+const a = [1, 2, 3, 4, 5];
 const item = _.filter(a, (items) => items > 4);
 console.log(item);
-$('h1').css({ color: 'blue' });
+
+$('h1').text('this is your ');
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
-var path = require("path");
+const path = require('path');
 module.exports = {
   entry: {
-    app: ["./app.js"]
+    'varsion1/v1': ["./app.js","./index.css"],
+    'version2/v2':["./app2.js","./index.css"]
+  /*  'stylish.js': ['./index.css'],*/
   },
-  
+  // 两个入口地址，webpack之后生成了两个文件，这两个文件在index.html中被引用了。
   output: {
-    
-    path: path.resolve(__dirname, "build"),
-    publicPath: "/",
-    filename: "bundle.js"
+
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: '[name].build.js',
   },
-   module: {
-  loaders: [
-            //.css 文件使用 style-loader 和 css-loader 来处理
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            {
+  module: {
+    loaders: [
+            // .css 文件使用 style-loader 和 css-loader 来处理
+       { test: /\.css$/, loader: 'style-loader!css-loader' },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',//在webpack的module部分的loaders里进行配置即可
+        loader: 'babel-loader', // 在webpack的module部分的loaders里进行配置即可
         query: {
-          presets: ['es2015']
-        }
-      }
-           
-        ]
-    }
+          presets: ['es2015'],
+        },
+      },
+
+    ],
+  },
 
 };

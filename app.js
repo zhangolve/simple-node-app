1a84852c2556540bc1725ed6eadc72b1fb32453d
zhangxingwang <zhangxingwang@sinosoft.com.cn>
2016-11-29 11:11:01 +0800

    修改index.html使localhost可访问

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 import $ from 'jquery';
 import _ from 'lodash' ;
 // import './index.css';
-const a = [1, 2, 3, 4, 5, 6, 7, 8];
+const a = [1, 2, 3, 4, 5, 6];
 const item = _.filter(a, (items) => items > 4);
 console.log(item);
-$('h1').css({ color: 'blue' });
+$('h1').css({ color: 'black' });
 
